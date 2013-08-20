b481bd9c063ca3ca2d25e7c41f098149bce118b2
Sebastián Nogara <snogaraleal@gmail.com>
2013-08-20 14:36:29 -0300

    Add buildAbsoluteUri to utils.

--- src/tatu/utils.js
+++ src/tatu/utils.js
@@ -1,11 +1,13 @@
 goog.provide('tatu.utils');
 
+goog.require('goog.Uri')
+
 
 /**
- *
- * @param uri
+ * Build absolute Uri using the window location as base Uri.
+ * @param uri Relative Uri.
+ * @return {string} Absolute Uri.
  */
-tatu.utils.absoluteUri = function(uri) {
-    // ./sample => http://www.examples.com/sample
-    //TODO: check js solution
+tatu.utils.buildAbsoluteUri = function(uri) {
+    return goog.Uri.resolve(goog.Uri.parse(window.location), goog.Uri.parse(uri)).toString();
 };
