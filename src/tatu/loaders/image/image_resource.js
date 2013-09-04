2534304b62b6912a5047efddedbefbd1c683e1a3
Sebastián Nogara <snogaraleal@gmail.com>
2013-09-04 10:13:24 -0300

    Fix image resource resolution.

--- src/tatu/loaders/image/image_resource.js
+++ src/tatu/loaders/image/image_resource.js
@@ -18,12 +18,25 @@ tatu.loaders.image.ImageResource = function(timeout, source) {
  * @param {function} resolve Resolution callback.
  */
 tatu.loaders.image.ImageResource.prototype.load = function(resolve) {
-    // TODO: Check with timeout
     this.image_ = document.createElement('img');
     this.image_.setAttribute('src', this.source_);
 
-    // TODO: Resolution
-    resolve();
+    // Resolution
+    if (!goog.isDef(this.source_) || this.source_.length == 0) {
+        resolve();
+    }
+    this.image_.onload = function() {
+        resolve();
+    };
+
+    // Check with timeout
+    setTimeout(goog.bind(function() {
+        if (!this.image_.complete ||
+            this.image_.naturalWidth === 'undefined' ||
+            this.image_.naturalWidth === 0) {
+            resolve();
+        }
+    }, this), this.timeout_);
 };
 
 
@@ -32,4 +45,5 @@ tatu.loaders.image.ImageResource.prototype.load = function(resolve) {
  */
 tatu.loaders.image.ImageResource.prototype.abort = function() {
     this.image_.removeAttribute('src');
+    delete this.image_;
 };
