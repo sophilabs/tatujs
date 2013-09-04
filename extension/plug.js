22bf595803b38af9a3417bf3b8fb817d836d39b0
Sebastián Nogara <snogaraleal@gmail.com>
2013-09-04 09:38:24 -0300

    Add per-tab activity to chrome extension.

--- extension/plug.js
+++ extension/plug.js
@@ -1,14 +1,33 @@
-var active = false;
+var activeTabs = [];
+var entriesCount = {};
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-    active = !active;
     var tabId = tab["id"];
 
-    chrome.browserAction.setBadgeText({
-        "text": "0"
-    });
+    var active = false;
+    var i;
+    for (i = 0; i < activeTabs.length; i++) {
+        if (activeTabs[i] == tabId) {
+            active = true;
+            break;
+        }
+    }
+
+    if (!active) {
+        activeTabs.push(tabId);
+        active = true;
+        entriesCount[tabId] = 0;
+    } else {
+        activeTabs.splice(i, 1);
+        active = false;
+    }
 
     if (active) {
+        chrome.browserAction.setBadgeText({
+            "text": entriesCount[tabId] + '',
+            "tabId": tabId
+        });
+
         chrome.tabs.executeScript(tabId, {
             "file": "tatu.js"
         }, function() {
@@ -17,22 +36,33 @@ chrome.browserAction.onClicked.addListener(function(tab) {
             }, function() {
                 chrome.runtime.onMessage.addListener(
                     function(request, sender, sendResponse) {
-                        chrome.browserAction.setBadgeText({
-                            "text": request["entries"]
-                        });
+                        entriesCount[sender.tab.id] = request["entries"];
+
+                        if (sender.tab.id == tabId) {
+                            chrome.browserAction.setBadgeText({
+                                "text": request["entries"],
+                                "tabId": tabId
+                            });
+                        }
                     }
                 );
             });
         });
         chrome.browserAction.setIcon({
-            "path": "icon_active.png"
+            "path": "icon_active.png",
+            "tabId": tabId
         });
     } else {
         chrome.tabs.executeScript(tabId, {
             "code": "window.location.reload();"
         });
         chrome.browserAction.setIcon({
-            "path": "icon.png"
+            "path": "icon.png",
+            "tabId": tabId
+        });
+        chrome.browserAction.setBadgeText({
+            "text": "",
+            "tabId": tabId
         });
     }
 });
